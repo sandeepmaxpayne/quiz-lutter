267270791ef7de665466624330aaefd618b59fe2
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2020-03-10 16:55:01 +0530

    quiz app

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'qizBrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = new QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizePage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizePage extends StatefulWidget {
+  @override
+  _QuizePageState createState() => _QuizePageState();
+}
+
+class _QuizePageState extends State<QuizePage> {
+  List<Icon> scoreKeeper = [];
+  int score = 0;
+  void checkAnswer(bool userPickAnswer) {
+    bool correctAnswer = quizBrain.getQuestionBankAnswer();
+    setState(() {
+      if (correctAnswer == userPickAnswer) {
+        score++;
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+
+      quizBrain.nextQuestion();
+
+      if (quizBrain.isFinished() == true) {
+        print(score);
+        Alert(
+            context: context,
+            title: 'Finished',
+            desc: 'Final Score: $score \nYou\'ve reached the end of quiz',
+            buttons: [
+              DialogButton(
+                color: Colors.yellowAccent,
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  'Close',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              )
+            ]).show();
+        quizBrain.reset();
+        scoreKeeper = [];
+        score = 0;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionBank(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/qizBrain.dart
@@ -0,0 +1,51 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+
+  List<Question> _questionBank = [
+    Question(q: 'You can lead a cow downstairs very easily.', a: false),
+    Question(q: 'Pollution is the main cause of global warming.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+    Question(q: 'Tom Marvolo Riddle was Lord Voldemort.', a: true),
+    Question(q: 'Sandeep Dutta was always an idiot.', a: false),
+    Question(
+        q: 'Touchstone from \'As you Like It\' was a motley fool.', a: true),
+    Question(q: 'Pinky Dutta was always an idiot.', a: true),
+    Question(q: 'Did earth blast in 2020.', a: false),
+    Question(q: 'Pig is dirty.', a: true),
+    Question(q: 'Chickens eat man.', a: false),
+    Question(q: 'Lucifer Morning Star is a hell God.', a: true),
+    Question(q: 'Hitler was a good dictator.', a: false),
+  ];
+
+  // make questionBank private and use encapsulation property
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+    print(_questionNumber);
+  }
+
+  String getQuestionBank() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getQuestionBankAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      print('returning true');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+}
